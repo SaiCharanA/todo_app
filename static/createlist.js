83af9e237d54d6ccf75a0273434f284c42e9e724
SaiCharan <saicharan@crowdspots.com>
2017-07-14 01:13:09 +0530

    added backend for signup

---
+++ static/createlist.js
@@ -0,0 +1,12 @@
+$(document).ready({
+function Edittask(id) {
+	var task_id = id;
+	var current_task = document.getElementbyId(task_id)
+	var updated_task = promt('Please update the task')
+	
+
+}
+
+function Deletetask(id) {
+	var task_id = id;
+}
--- todo.py
+++ todo.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from flask_mail import Mail, Message
 import sqlite3
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'adavenisaicharan@gmail.com'
+app.config['MAIL_PASSWORD'] = 'sai1020143151'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 @app.route ("/")
 def index():
@@ -19,6 +28,52 @@ def signin():
 def createlist():
 	return render_template("createlist.html")
 
+@app.route("/forgotpassword")
+def forgotpassword():
+	return render_template("forgotpassword.html")
+
+
+######################################################################
+#@app.route("/emailpassword",methods=['POST'])
+#def emailpassword():
+#	try:
+#		conn = sqlite3.connect(todolist.db)
+#		cur = conn.cursor
+#		email = request.form["email"]
+
+
+#		msg = "Your password to sign in into todo app is" + pwd
+#	return ('sent')
+######################################################################
+@app.route("/signupdetails",methods=['POST'])
+def signupdetails():
+	try:
+		conn = sqlite3.connect("todolist.db")
+		cur = conn.cursor()
+		username = request.form["username"]
+		email = request.form["email"]
+		password = request.form["password"]
+		cur.execute("insert into signup (username,email,password) values (?,?,?)", [username,email,password])
+		conn.commit()
+		msg  = "User createsd successfully"
+	except:
+		conn.rollback()
+		msg = "Error in creating user"
+	finally:
+		flash (msg)
+		return render_template("signin.html")
+
+######################################################################
+#@app.route("/signindetails",methods=['POST'])
+#def signindetails():
+#	try:
+#		conn = sqlite3.connect("todolist.db")
+#		cur = conn.cursor()
+#		email = request.form["username"]
+#		pwd = request.form["password"]
+
+#		return 'login successful'
+######################################################################
 @app.route("/tasks",methods=['POST'])
 def tasks():
 	try:
@@ -35,17 +90,38 @@ def tasks():
 		return render_template("result.html",msg = msg)
 		conn.close()
 
+#######################################################################
 @app.route("/createdlist")
 def createdlist():	
-   		con = sqlite3.connect("todolist.db")
-   		con.row_factory = sqlite3.Row
+   		conn = sqlite3.connect("todolist.db")
+   		conn.row_factory = sqlite3.Row
    		
-   		cur = con.cursor()
+   		cur = conn.cursor()
    		cur.execute("select * from list")
     	
 		rows = cur.fetchall()
    		return render_template("createdlist.html",rows = rows)
-   		con.close()
+   		conn.close()
+
+#######################################################################
+#@app.route("/edittask")
+#def edittask():
+
+
+
+
+
+
+
+
+#		return ('task updated')
+
+#######################################################################
+#@app.route("/deletetask")
+#def deletetask():
+
+
+#		return ('task deleted')
 
 
 if __name__ == "__main__ ":
